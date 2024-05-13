8d7548e311868847dd5da7d18be33ae774c19492
WOOJINLEEdev <lwj3794@naver.com>
2024-05-13 19:44:54 +0900

    chore: 주석 삭제

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { ChangeEvent, KeyboardEvent, useEffect, useRef, useState } from "react";
 
 import { useForm } from "react-hook-form";
